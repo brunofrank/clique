affc0c147b853c447ad43754a473e63ea797a191
Bruno Frank <bfscordeiro@gmail.com>
2022-08-27 10:41:46 -0300

    :sparkles: Project setup
    Adding Devise, rspec and seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+100.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, password: "123123")
+end
