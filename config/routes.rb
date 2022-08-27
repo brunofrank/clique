3a969c781721645bf8fc148e7dcc6ba6aa50a4d5
Bruno Frank <bfscordeiro@gmail.com>
2022-08-27 10:44:44 -0300

    :sparkles: Adding groups controller
    Adding Groups resource and remove the home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'groups#index'
+
   devise_for :users
+
+  resources :groups
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_131914) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_134347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.bigint "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_groups_on_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -27,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_131914) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users", column: "owner_id"
 end
